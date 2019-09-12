f6f1523c4fa1fb656826a48ee091d827c65c3de3
5484f90d2e8e272e31edc5044ff2b20b660d21d9 <marcusguttenplan@gmail.com>
2019-09-12 10:23:56 -0700

    update

--- bootstrappers/cloud-lab/_providers.tf
+++
@@ -1,7 +0,0 @@
-# Specify whether to use GCP, Azure, AWS, etc. We will use GCP.
-provider "google" {
-  # credentials = "${env.TF_CREDS}"
-  credentials = "${file("./creds.json")}"
-  project = "${var.gcloud-project}"
-  region = "${var.gcloud-region}"
-}
