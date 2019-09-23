b6c1de9c2606889a90580510f0e45b1f0ac5998c
5484f90d2e8e272e31edc5044ff2b20b660d21d9 <marcusguttenplan@gmail.com>
2019-09-23 10:02:11 -0400

    add php dockerfile

---
+++ dockerfiles/ui-php/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<title>Sparks Creative Technology</title>
+    <!-- <link rel="stylesheet" href="css/fonts/fonts.css?<?php echo strtotime('now');?>" type="text/css"> -->
+</head>
+
+<body>
+
+    <main>
+        <h1>
+        Welcome</h1>
+    </main>
+
+		<!-- <script src="./js/index.js?<?php echo strtotime('now');?>" ></script> -->
+</body>
+
+</html>
