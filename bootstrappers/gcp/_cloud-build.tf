e8bbbb93da8c77c2c5720050e120b880c6f7613b
5484f90d2e8e272e31edc5044ff2b20b660d21d9 <marcusguttenplan@gmail.com>
2020-03-24 16:15:26 -0400

    add dev pipeline

--- bootstrappers/gcp/_cloud-build.tf
+++ bootstrappers/gcp/_cloud-build.tf
@@ -6,5 +6,17 @@ resource "google_cloudbuild_trigger" "image" {
     repo_name   = google_sourcerepo_repository.repo.name
   }
 
-  filename = "cloudbuild.yaml"
+  filename = "cloudbuild_prod.yaml"
+}
+
+
+resource "google_cloudbuild_trigger" "image" {
+  project = google_sourcerepo_repository.repo.project
+
+  trigger_template {
+    branch_name = "dev"
+    repo_name   = google_sourcerepo_repository.repo.name
+  }
+
+  filename = "cloudbuild_dev.yaml"
 }
