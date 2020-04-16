7d078c6c9a111ea51244870b56b69bfbaa5a6203
5484f90d2e8e272e31edc5044ff2b20b660d21d9 <marcusguttenplan@gmail.com>
2020-04-16 09:25:14 -0400

    add cloud build configs

--- bootstrappers/gcp/_providers.tf
+++ bootstrappers/gcp/_providers.tf
@@ -1,6 +1,5 @@
 # Specify whether to use GCP, Azure, AWS, etc. We will use GCP.
 provider "google" {
-  # credentials = "${env.TF_CREDS}"
   credentials = "${file("./creds.json")}"
   project = "${var.project}"
   region = "${var.region}"
