84f2e4c53e6e46b26da35efbe63e00f2c66f7c57
5484f90d2e8e272e31edc5044ff2b20b660d21d9 <marcusguttenplan@gmail.com>
2020-04-29 11:41:23 -0400

    update express

--- servers/express/express.js
+++ servers/express/express.js
@@ -1,17 +1,27 @@
-// npm i express cors
-
 const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+const cors = require('cors');
 const app = express();
 const server = require('http').createServer(app);
-const cors = require('cors');
 
-app.use(cors());
+// parse requests with bodyparser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// CORS
+app.use(cors({origin: '*'}));
+
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/', function(req, res, next) {
-    res.sendFile(__dirname + '/index.html')
+    res.status(200).send({
+        message: "/",
+    });
 });
 
 const port = process.env.PORT || 3000;
 server.listen(port);
-console.log(`Server listening on http://localhost:${port}`);
+console.log(`Server listening on ${port}`);
